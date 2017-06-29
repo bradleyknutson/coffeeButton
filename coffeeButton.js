6e753d7cdc369c429dfc9b1f4b2fdc8bbb565906
bknutson <bradley@bknutson.com>
2017-06-29 22:25:30 +0000

    Modified push to release to avoid issue with code being triggered upon starting robot

--- coffeeButton.js
+++ coffeeButton.js
@@ -1,6 +1,6 @@
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-var slackOut = require('/home/pi/Documents/coffee/coffeeOut.json');
-var slackIn = require('/home/pi/Documents/coffee/coffeeIn.json');
+var slackOut = require('/home/pi/Documents/coffeeButton/coffeeOut.json');
+var slackIn = require('/home/pi/Documents/coffeeButton/coffeeIn.json');
 var Cylon = require('cylon');
 
 Cylon.robot({
@@ -18,18 +18,18 @@ work: function(my) {
     var xhr = new XMLHttpRequest();
     var url = 'https://hooks.slack.com/services/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';
 
-    my.redButton.on('push', function() {
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    var data = JSON.stringify(slackOut);
-    xhr.send(data); 
+    my.redButton.on('release', function() {
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        var data = JSON.stringify(slackOut);
+        xhr.send(data); 
     });
 
-    my.blueButton.on('push', function() {
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    var data = JSON.stringify(slackIn);
-    xhr.send(data);
+    my.blueButton.on('release', function() {
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        var data = JSON.stringify(slackIn);
+        xhr.send(data);
     });
 }
 
